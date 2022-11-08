5a05b951c371f9e060cb0f979fc11d2beb2f9660
abdullah-mamun369 <maamamun369@gmail.com>
2022-11-08 23:15:17 +0600

    Confirmation order has created

---
+++ src/Pages/ConfirmationOrder/ConfirmationOrder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ConfirmationOrder.css'
+
+const ConfirmationOrder = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ConfirmationOrder;
